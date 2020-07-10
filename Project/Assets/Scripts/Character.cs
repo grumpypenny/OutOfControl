cf4b9ebf3c1268f8903fbdd4483b2647d731694f
Faaris Ali <faarisali111@gmail.com>
2020-07-10 18:42:53 -0400

    Added base stats to character class

--- Project/Assets/Scripts/Character.cs
+++ Project/Assets/Scripts/Character.cs
@@ -4,27 +4,41 @@ using UnityEngine;
 // This class is for any game object that needs stats such as health, defence, and offence
 public class Character : MonoBehaviour, ICharacter<int>
 {
+    // Base stats
     public int startHealth;
     protected int health;
 
+    public int startBaseDefence;
+    protected int baseDefence;
+
+    public int startBaseOffence;
+    protected int baseOffence;
+
+    // Base stat multipliers
     public int startDefence;
     protected int defence;
 
     public int startOffence;
     protected int offence;
 
+    // Character states
     protected bool dead;
 
     // Start is called before the first frame update
     public virtual void Start()
     {
         health = startHealth;
+
+        baseDefence = startBaseDefence;
+        baseOffence = startBaseOffence;
+
         defence = startDefence;
         offence = startOffence;
 
         dead = false;
     }
 
+    // Character takes damage damage
     public void TakeHit(int damage)
     {
         health -= damage;
@@ -34,11 +48,13 @@ public class Character : MonoBehaviour, ICharacter<int>
         }
     }
 
+    // Character gains health health
     public void Heal(int health)
     {
         this.health += health;
     }
 
+    // Setters for base stat multipliers
     public void SetOffence(int offence)
     {
         this.offence = offence;
@@ -49,8 +65,11 @@ public class Character : MonoBehaviour, ICharacter<int>
         this.defence = defence;
     }
 
+    // Method executed on death of character
     public void Die()
     {
         dead = true;
+        GameObject.Destroy(gameObject);
     }
+
 }
