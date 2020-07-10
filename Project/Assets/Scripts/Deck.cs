0170aae27447e8b6595eead68540cb26eb762dda
grumpypenny <ajitesh.misra.14@gmail.com>
2020-07-10 19:34:30 -0400

    Added Open and Closing Deck Animations

---
+++ Project/Assets/Scripts/Deck.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Animations;
+
+[RequireComponent(typeof(Animator))]
+public class Deck : MonoBehaviour
+{
+	private Animator anim;
+	private bool open = false;
+
+	public void Start()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	public void ToggleDeck()
+	{
+		open = !open;
+		print(open);
+		anim.SetBool("open", open);
+	}
+}
+
