1de4a3e1a15f9f7ed8255bbf313d3145c15e25cd
Faaris Ali <faarisali111@gmail.com>
2020-07-10 18:07:28 -0400

    Implemented simple character class

--- Project/Assets/Scripts/Character.cs
+++ Project/Assets/Scripts/Character.cs
@@ -2,17 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character : MonoBehaviour
+// This class is for any game object that needs stats such as health, defence, and offence
+public class Character : MonoBehaviour, ICharacter<int>
 {
+    public int startHealth;
+    protected int health;
+
+    public int startDefence;
+    protected int defence;
+
+    public int startOffence;
+    protected int offence;
+
+    protected bool dead;
+
     // Start is called before the first frame update
-    void Start()
+    protected virtual void Start()
     {
-        
+        health = startHealth;
+        defence = startDefence;
+        offence = startOffence;
+
+        dead = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeHit(int damage)
     {
-        
+        health -= damage;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(int health)
+    {
+        this.health += health;
+    }
+
+    public void SetOffence(int offence)
+    {
+        this.offence = offence;
+    }
+
+    public void SetDefence(int defence)
+    {
+        this.defence = defence;
+    }
+
+    public void Die()
+    {
+        dead = true;
     }
 }
--- Project/Assets/Scripts/ICharacter.cs
+++ Project/Assets/Scripts/ICharacter.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // Interface for damageable game objects
-public interface IDamageable<T>
+public interface ICharacter<T>
 {
     void TakeHit(T damage);
+    void Heal(T health);
+    void SetOffence(T offence);
+    void SetDefence(T defence);
 }
