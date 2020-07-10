9b6c93f0b17c3a91ef24a43c99aef5ddb1f9cd6d
Faaris Ali <faarisali111@gmail.com>
2020-07-10 17:44:01 -0400

    Made a Scripts folder and started a character script

---
+++ Project/Assets/Scripts/IDamageable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Interface for damageable game objects
+public interface IDamageable<T>
+{
+    void TakeHit(T damage);
+}
