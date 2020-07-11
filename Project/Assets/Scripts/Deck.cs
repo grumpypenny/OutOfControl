e5e071b17d39be040a1377ee0d1d1741657afee2
grumpypenny <ajitesh.misra.14@gmail.com>
2020-07-11 14:53:58 -0400

    UI now spawns cards and can execute them
    Done with the hard part

--- Project/Assets/Scripts/Deck.cs
+++ Project/Assets/Scripts/Deck.cs
@@ -10,23 +10,33 @@ public class Deck : MonoBehaviour
 	public Button[] grid;
 
 	private Animator anim;
-	private bool open = false;
 	private int cardCount = 0;
+
+	private GameManager gm;
 
 	public void Start()
 	{
 		anim = GetComponent<Animator>();
+		gm = FindObjectOfType<GameManager>();
 	}
 
 	public void ToggleDeck()
 	{
-		open = !open;
-		anim.SetBool("open", open);
+		bool nextStatus = !anim.GetBool("open");
+		anim.SetBool("open", nextStatus);
 	}
 
 	public void SelectCard(string type)
 	{
 		cardCount++;
+
+		// send in the cards to execute
+		if (gm != null)
+		{
+			CardType ct = (CardType)System.Enum.Parse(typeof(CardType), type);
+
+			gm.AddCard(ct);
+		}
 
 		if (cardCount >= 5)
 		{
@@ -35,6 +45,13 @@ public class Deck : MonoBehaviour
 				b.interactable = false;
 			}
 			cardCount = 0;
+
+			if (gm != null)
+			{
+				gm.Execute();
+				anim.SetBool("open", false);
+				return;
+			}
 		}
 	}
 
@@ -48,6 +65,12 @@ public class Deck : MonoBehaviour
 		foreach (Button b in grid)
 		{
 			b.interactable = true;
+		}
+
+		// reset the amount of cards
+		if (gm != null)
+		{
+			gm.ResetCards();
 		}
 	}
 }
---
+++ Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Tracks All entities in the game along with actions
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+	public List<Card> turnActions;
+	public GameObject CardObject;
+	public Transform[] CardPos;
+	private int index = 0;
+
+	// Add a card to the player actions
+	public void AddCard(CardType type)
+	{
+		print(type);
+
+		if (index > CardPos.Length)
+		{
+			Debug.LogError("Not Enough Card Pos");
+			return;
+		}
+
+		// spawn card as child of positon
+		Instantiate(CardObject, CardPos[index]);
+		index++;
+	}
+
+	// Remove all cards from player actions
+	// and remove them from world space
+	public void ResetCards()
+	{
+		turnActions.Clear();
+		index = 0;
+		foreach (Transform t in CardPos)
+		{
+			ArrayList children = new ArrayList();
+			foreach(Transform child in t)
+			{
+				children.Add(child);
+			}
+
+			foreach(Transform c in children)
+			{
+				Destroy(c.gameObject);
+			}
+		}
+	}
+
+	// apply card effects
+	public void Execute()
+	{
+		print("executing cards");
+	}
+}
